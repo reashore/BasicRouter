841e545a2b04206b39451cbfbc77cbe794562ad8
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-14 12:15:57 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const reactContainer = document.getElementById('root');
+ReactDOM.render(<App />, reactContainer);
+
+registerServiceWorker();
